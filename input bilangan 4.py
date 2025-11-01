00693cef27e858567f11ea0494b3ebd19bc88ca7
MAULANA ISNAN IBRAHIM <maulanaisnanibrahim@gmail.com>
2025-11-01 20:12:57 +0700

    Add files via upload

---
+++ input bilangan 4.py
@@ -0,0 +1,15 @@
+#Masukan input 
+bil1 = int (input("Masukan bilangan : "))
+bil2 = int (input("Masukan bilangan : "))
+bil3 = int (input("Masukan bilangan : "))
+bil4 = int (input("Masukan bilangan : "))
+
+#Nilai terbesar
+
+if (bil4 > bil1):
+   print("Bilangan terbesar :",bil4)
+
+#Nilai terkecil
+
+if (bil1 < bil4):
+   print("Bilangan terkecil :",bil1)
